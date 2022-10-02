9e2c3f44891ca2b0f055c392a71dfb046458e60f
YaroslavZagura <106350840+YaroslavZagura@users.noreply.github.com>
2022-10-02 18:24:45 +0300

    add homework 02

---
+++ index.js
@@ -0,0 +1,8 @@
+const user= prompt("Як Вас звати?","");
+const age= prompt("Cкільки Вам років?","");
+const region= prompt("Звідки Ви родом?","");
+alert("Привіт, "+user+"!"+" Вам вже "+age+" років! "+region+" - ваша прекрасна домівка! Але як щодо навколосвітніх пригод?");
+let number = 12345;
+let string = String(number);
+let arr = string.split('').join(" ");
+console.log(arr);
